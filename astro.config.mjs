99a2bdfe421c155e0cc932872be17f6ae8032ff9
Mauricio M <mcm.crw@gmail.com>
2023-08-15 00:17:28 -0400

    feat: Game of life

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,17 @@ import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), vue()],
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
-  },
+      external: ["svgo"]
+    }
+  }
 });
---
+++ src/components/vue/Card.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    Change Everything
+  </div>
+</template>
+<script setup lang="ts">
+</script>
