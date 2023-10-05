a230cfb1ef561bffc345a4c163b8cc4eb0c8a58b
Mauricio M <mcm.crw@gmail.com>
2023-10-05 00:05:34 -0400

    images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hackmeeting-2023.vercel.app/',
   // site: "https://hackmeeting.org.bo",
-  integrations: [tailwind(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), vue()],
+  integrations: [
+    tailwind(), 
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp"
+    }),
+    vue()
+  ],
   vite: {
     ssr: {
       external: ["svgo"]
