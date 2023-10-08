9d9fcb1a4afefc85db437253b63713fa3761f8a2
Mauricio M <mcm.crw@gmail.com>
2023-10-08 18:03:46 -0400

    feat, footer

---
+++ src/components/information-section.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="grid grid-cols-2">
+    <div class="flex justify-center items-center h-72 col-span-2 md:col-span-1">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3807.2430495624035!2d-66.17065831466132!3d-17.400119821952615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x93e3738cd6116dad%3A0xce0932c489920745!2sProyecto%20mARTadero!5e0!3m2!1sen!2sbo!4v1696801541613!5m2!1sen!2sbo" width="100%" height="100%" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+    <div class="relative w-full h-72 col-span-2 md:col-span-1">
+      <div class="absolute bg-black opacity-80 w-full h-full"></div>
+      <img src="/images/martadero_space.jpg" alt="martadero sede del evento" class="w-full h-full object-cover">
+      <div class="absolute w-full h-full flex flex-col justify-center top-0 p-4 md:p-8 text-xs md:text-sm">
+        <div class="text-lg text-primary font-semibold">Contáctanos</div>
+        <div class="text-xs my-2 font-light">¿Tienes preguntas, sugerencias, dudas, propuestas o consultas? Escríbenos</div>
+        <div>
+          <span class="text-primary">
+            Email:
+          </span>
+          hm2023-staff@hackmeeting.org.bo
+        </div>
+        <div>
+          <span class="text-primary">
+            Dirección del Evento:
+          </span>
+          Calle 27 de Agosto esquina Ollantay
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
